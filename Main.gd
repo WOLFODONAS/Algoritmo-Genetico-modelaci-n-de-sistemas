0b189f7beac323df671e2cf195d09bae535442e3
Don Donas <marcoseduardo.meas@gmail.com>
2024-04-25 13:46:00 -0600

    V1

--- Main.gd
+++ Main.gd
@@ -4,16 +4,25 @@ var x:int #Matrix x size
 var y:int #Matriz y size
 var p:int #Population
 var g:int #Generation
+var ga:int #Gasp Amount
+var betterMatrix:Array[String]
+var betterFitness:int = -1
 const  chars = "abcdefghijklmnopqrstuvwxyz"
 var sexMode:int
+signal  endProsses
 @export var matrixLabel:Label
 @export var outputLable:RichTextLabel
 
 
 func _on_button_pressed() -> void:
+	betterFitness = -1
+	betterMatrix = []
+	$VSplitContainer/Panel2/HBoxContainer/VBoxContainer/Control/Button.disabled = true
+	await  get_tree().process_frame
+	await  get_tree().process_frame
+	await  get_tree().process_frame
 	matrix.clear()
 	matrix.resize(y)
-	
 	for i in y:
 		var newString = ""
 		var pchars = chars.substr(0,x)
@@ -29,7 +38,7 @@ func _on_button_pressed() -> void:
 	outputLable.clear()
 	population.resize(p)
 	for i in p:
-		population[i] = Individual.new(x,y)
+		population[i] = Individual.new(x,y,_get_random_choromosomes(x*y))
 		population[i].evaluete(matrix)
 	_live(1,population)
 
@@ -37,6 +46,9 @@ func _live(generation:int,population:Array[Individual]):
 	
 	population.sort_custom(sort_by_fitness)
 	var  bestFitness:int= population[0].fitness
+	if bestFitness>betterFitness:
+		betterFitness = bestFitness
+		betterMatrix = population[0].get_matrix(matrix)
 	var  worstFitness:int = population[p-1].fitness
 	outputLable.append_text("Generation: [color=blue]%s[/color]\n"%generation)
 	outputLable.append_text("Best Fitness: [color=%s]%s[/color]\n"%["red"if bestFitness>float(x*y)/2 
@@ -44,6 +56,7 @@ func _live(generation:int,population:Array[Individual]):
 	outputLable.append_text("Worst Fitness: [color=%s]%s[/color]\n"%["red"if worstFitness>float(x*y)/2 
 	else "yellow" if worstFitness != 0 else "green",worstFitness])
 	if  generation>=g:
+		emit_signal("endProsses")
 		outputLable.append_text(str(population[0].chromosomes)+"\n")
 		for i in population[0].get_matrix(matrix):
 			outputLable.append_text(str(i)+"\n")
@@ -86,16 +99,16 @@ func _live(generation:int,population:Array[Individual]):
 func _get_better_kid(mother:Individual,father:Individual):
 	var matrixSize=(x*y)
 	var crossLine:int = int(randf() * matrixSize)
-	var cA:Array[bool]=(mother.chromosomes.slice(0,crossLine) 
+	var cA:Array[int]=(mother.chromosomes.slice(0,crossLine) 
 						+ father.chromosomes.slice(crossLine,matrixSize))
-	var cB:Array[bool]=(father.chromosomes.slice(0,crossLine) 
+	var cB:Array[int]=(father.chromosomes.slice(0,crossLine) 
 						+ mother.chromosomes.slice(crossLine,matrixSize))
 	var randomGenA = int(randf() * matrixSize)
 	var randomGenB = int(randf() * matrixSize)
-	cA[randomGenA] = !cA[randomGenA] 
-	cB[randomGenB] = !cB[randomGenB] 
-	var son:Individual = Individual.new(x,y,false,cA)
-	var daughter:Individual = Individual.new(x,y,false,cB)
+	cA[randomGenA] = int(randf() * ga)
+	cB[randomGenB] = int(randf() * ga)
+	var son:Individual = Individual.new(x,y,cA)
+	var daughter:Individual = Individual.new(x,y,cB)
 	son.evaluete(matrix)
 	daughter.evaluete(matrix)
 	return son if son.fitness>daughter.fitness else daughter
@@ -117,57 +130,52 @@ func _on_x_value_changed(val: int) -> void:
 func _on_y_value_changed(val: int) -> void:
 	y=val
 
+
+func _on_gasp_amount_value_changed(val) -> void:
+	ga = val
+
 func _on_option_button_item_selected(index: int) -> void:
 	sexMode = index
 	
 	$VSplitContainer/Panel2/HBoxContainer/VBoxContainer/Control/Button.disabled = false
-
-
+func _get_random_choromosomes(sz:int) -> Array[int]:
+	var theC:Array[int]=[]
+	for i in sz:
+		theC.append(int(randf() *(ga+1)))
+	return theC
 
 class Individual:
-	var chromosomes:Array[bool]
+	var chromosomes:Array[int]
 	var fitness:int =0
-	var _x:int
-	var _y:int
-	func _init(cx:int,cy:int,
-				isRandom:bool=true,newChromosomes:Array[bool]=[]) -> void:
-		_x = cx
-		_y = cy
-		if isRandom or newChromosomes.size()!=cx*cy:
-			for i in cy*cx:
-				chromosomes.append(randi() % 2==0)
-		else :
-			for i in cy:
-				chromosomes = newChromosomes
+	var x:int
+	var y:int
+	func _init(_x:int,_y:int,_chromosomes:Array[int]=[]) -> void:
+		x = _x
+		y = _y
+		chromosomes = _chromosomes
 	func get_matrix(matrix:Array[String])->Array[String]:
 		var trueMatrix:Array[String]=[]
-		var xx = _x*2
-		for i in _y:
+		for i in y:
 			var row = ""
 			var chars = matrix[i]
-			var startPos = i*_y
-			var chromosomesRow:Array[bool] = chromosomes.slice(startPos,startPos+_x)
-			
-			for j in _x:
-				
-				if chromosomesRow[j]:
-					row += "_"
-				else:
-					row+=chars[0]
-					chars = chars.substr(1)
-			row+=chars
-			for j in xx - row.length():
-				row += "_"
+			var startPos = i*x
+			var chromosomesRow:Array[int] = chromosomes.slice(startPos,startPos+x)
+			for j in x:
+				row += "_".repeat(chromosomesRow[j])+chars[j]
 			trueMatrix.append(row)
 		return trueMatrix
 	func evaluete(matrix:Array[String]):
 		fitness=0
 		var trueMatrix:Array[String] = get_matrix(matrix)
-		print(trueMatrix)
-		for i in _x*2:
+		var indx:int=0
+		while true:
+			var toContinue:bool = false
 			var chars:Dictionary ={}
-			for j in _y:
-				var theChar:String = trueMatrix[j][i]
+			for i in y:
+				if trueMatrix[i].length()<=indx:
+					continue
+				toContinue=true
+				var theChar:String = trueMatrix[i][indx]
 				if theChar == "_":
 					continue
 				if theChar in chars.keys():
@@ -175,5 +183,16 @@ class Individual:
 					continue
 				chars[theChar] =1
 			if chars.size()==1:
-				fitness+=chars[chars.keys()[0]]
-				#fitness+=1
+				var amnt = chars[chars.keys()[0]]
+				if amnt >1:
+					amnt =int( pow(amnt,2))
+				fitness+=amnt
+			if !toContinue:
+				break
+			indx+=1
+
+
+func _on_end_prosses():
+	$Window.popup()
+	$Window/Control.set_info(matrix,betterMatrix)
+	$VSplitContainer/Panel2/HBoxContainer/VBoxContainer/Control/Button.disabled = false
---
+++ drawMatrix.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+var startMatrix:Array[String]
+var endMatrix:Array[String]
+var sqS:int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_info(_startMatrix:Array[String],_endMatrix:Array[String]) ->void:
+	startMatrix = _startMatrix
+	endMatrix = _endMatrix
+	queue_redraw()
+
+func _draw() -> void:
+	for i in startMatrix.size():
+		for j in startMatrix[i].length():
+			var startPoint = Vector2(j,i)*sqS
+			var endPoint = Vector2.ONE*sqS
+			var ltSize = int(float(sqS/2.0))
+			draw_rect(Rect2(startPoint,endPoint),Color.RED,false,4)
+			draw_char(get_theme_default_font(),startPoint+ (endPoint/2),startMatrix[i][j],ltSize)
+	var endStartM:int = startMatrix.size()+1
+	print(endStartM)
+	for i in endMatrix.size():
+		for j in endMatrix[i].length():
+			var startPoint = (Vector2(j,i+endStartM)*sqS) 
+			print(startPoint)
+			var endPoint = Vector2.ONE*sqS
+			var ltSize = int(float(sqS/2.0))
+			draw_rect(Rect2(startPoint,endPoint),Color.LAWN_GREEN,false,4)
+			draw_char(get_theme_default_font(),startPoint+ (endPoint/2),endMatrix[i][j],ltSize)
+
+
+
+
+func _on_squere_size_value_changed(val)-> void:
+	sqS = val
+	if endMatrix != null:
+		queue_redraw()
